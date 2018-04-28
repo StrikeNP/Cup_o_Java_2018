973d815e1c776f1ff6cd75ed4d5f0541aee8c4b3
andy-wojciechowski <31997141+andy-wojciechowski@users.noreply.github.com>
2018-04-28 11:41:37 -0500

    Implemented a method which helped to form the map

--- src/cupojava/problemone/Node.java
+++ src/cupojava/problemone/Node.java
@@ -3,8 +3,14 @@ package cupojava.problemone;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Node {
-    Node N = null, S = null, E = null, W = null, NE = null, NW = null, SE = null, SW = null;
+public class Node implements GridElement {
+
+    private int player = 0;
+
+    public static final int N = 0, S = 1, E = 2, W = 3, NE = 4, NW = 5, SE = 6, SW = 7;
+
+    protected Node[] neighbors = new Node[8];
+//    Node N = null, S = null, E = null, W = null, NE = null, NW = null, SE = null, SW = null;
 
 
 
@@ -12,7 +18,7 @@ public class Node {
     public List<Node> getConnections() {
         List<Node> matches = new ArrayList<>();
         for(int i = 0; i < neighbors.length; i++) {
-            if(player = neighbors[i]) {
+            if(player == neighbors[i].player) {
                 matches.add(neighbors[i]);
             }
         }
@@ -26,4 +32,32 @@ public class Node {
     public void clear() {
         this.player = 0;
     }
+
+
+    public Node(int player, Node[] neighbors){
+        this.player = player;
+        this.neighbors = neighbors;
+    }
+
+    public void validateBorders() {
+        for(Node testNode: neighbors){
+
+        }
+    }
+
+
+    private void createColumn(Node edgeNode, boolean createOnLeft){
+//        do{
+//            Node newNode = new Node(0, )
+//        }
+        Node nextNode = edgeNode.neighbors[N];
+        while(nextNode != null){
+//                nextNode.neighbors[E] =
+        }
+    }
+    private void createRow(){
+
+    }
+
+
 }
