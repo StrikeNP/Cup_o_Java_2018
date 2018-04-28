453b7bdc652a3a033efdf1ad9aef1af51bb813bf
George Burkhardt <burkhardtr@msoe.edu>
2018-04-28 11:06:40 -0500

    Added getConnections(), setPlayer() and clear() to the Node class

--- src/cupojava/problemone/Node.java
+++ src/cupojava/problemone/Node.java
@@ -1,7 +1,29 @@
 package cupojava.problemone;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
     Node N = null, S = null, E = null, W = null, NE = null, NW = null, SE = null, SW = null;
 
 
+
+
+    public List<Node> getConnections() {
+        List<Node> matches = new ArrayList<>();
+        for(int i = 0; i < neighbors.length; i++) {
+            if(player = neighbors[i]) {
+                matches.add(neighbors[i]);
+            }
+        }
+        return matches;
+    }
+
+    public void setPlayer(int player) {
+        this.player = player;
+    }
+
+    public void clear() {
+        this.player = 0;
+    }
 }
