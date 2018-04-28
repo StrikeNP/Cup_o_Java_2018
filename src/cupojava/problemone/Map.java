4fb8194965e903bc81f295a4c6b3c16e34a6b2a4
leel <leel@msoe.edu>
2018-04-28 11:23:02 -0500

    Started to make helper methods inside the Map class

--- src/cupojava/problemone/Map.java
+++ src/cupojava/problemone/Map.java
@@ -1,16 +1,33 @@
 package cupojava.problemone;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Map {
+    //private static GridElement firstPosition;
+    private static Node firstPosition;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        firstPosition = new Node();
         int numPlayers = Integer.parseInt(in.nextLine());
         int numMoves = Integer.parseInt(in.nextLine());
         ArrayList<String> moves = new ArrayList<>();
         for(int i = 0; i < (numMoves * numPlayers); i++) {
             moves.add(in.nextLine());
         }
+        for (int i =0; i < numPlayers; i++){
+
+        }
+    }
+
+    private static void formConnections(List<String> moves) {
+        //TODO: From the connections based on the moves
+    }
+
+    private static int calculateScore(int playerNumber){
+        //TODO: Use getConnections() to calculate the score
+        return 0;
     }
 }
