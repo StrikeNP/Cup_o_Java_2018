74e77d2342f8eda94433960682039fa0bfe384b3
George Burkhardt <burkhardtr@msoe.edu>
2018-04-28 10:35:49 -0500

    Added input management

--- src/cupojava/problemone/Map.java
+++ src/cupojava/problemone/Map.java
@@ -1,4 +1,16 @@
 package cupojava.problemone;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Map {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numPlayers = Integer.parseInt(in.nextLine());
+        int numMoves = Integer.parseInt(in.nextLine());
+        ArrayList<String> moves = new ArrayList<>();
+        for(int i = 0; i < (numMoves * numPlayers); i++) {
+            moves.add(in.nextLine());
+        }
+    }
 }
