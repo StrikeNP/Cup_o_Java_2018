f1458a3ee7acba6710183bf44bde4c43ebd6c257
andy-wojciechowski <31997141+andy-wojciechowski@users.noreply.github.com>
2018-04-28 12:28:42 -0500

    Implemented some additional changes in the map class

--- src/cupojava/problemone/Map.java
+++ src/cupojava/problemone/Map.java
@@ -68,7 +68,62 @@ public class Map {
     }
 
     private static int calculateScore(int playerNumber){
-        //TODO: Use getConnections() to calculate the score
-        return 0;
+        int currentScore = 0;
+        List<TraversalNode> nodesToTraverse = getNodesFromPlayer((Node)firstPosition, playerNumber, new ArrayList<>());
+        for(TraversalNode traversalNode : nodesToTraverse) {
+            if(!traversalNode.isChecked) {
+                List<Integer> numberOfNodesInARow = new ArrayList<>();
+                numberOfNodesInARow.add(calculateScore(traversalNode.node, 0, Node.N, playerNumber));
+                numberOfNodesInARow.add(calculateScore(traversalNode.node, 0, Node.S, playerNumber));
+                numberOfNodesInARow.add(calculateScore(traversalNode.node, 0, Node.E, playerNumber));
+                numberOfNodesInARow.add(calculateScore(traversalNode.node, 0, Node.W, playerNumber));
+                numberOfNodesInARow.add(calculateScore(traversalNode.node, 0, Node.NE, playerNumber));
+                numberOfNodesInARow.add(calculateScore(traversalNode.node, 0, Node.NW, playerNumber));
+                numberOfNodesInARow.add(calculateScore(traversalNode.node, 0, Node.SE, playerNumber));
+                numberOfNodesInARow.add(calculateScore(traversalNode.node, 0, Node.SW, playerNumber));
+
+                long numberOfPoints = numberOfNodesInARow.stream().filter(x -> x >= 3).count();
+                for(int i = 0; i < numberOfPoints; i++) {
+                    currentScore++;
+                }
+            }
+        }
+        return currentScore;
+    }
+
+    private static int calculateScore(Node currentNode, int numberOfNodesWithSamePlayer, int currentDirection, int playerNumber) {
+        if(currentNode == null) {
+            return numberOfNodesWithSamePlayer;
+        } else {
+            int localNumOfNodes = numberOfNodesWithSamePlayer;
+            if(currentNode.getPlayer() == playerNumber) {
+                localNumOfNodes++;
+            }
+            return calculateScore(currentNode.neighbors[currentDirection], localNumOfNodes, currentDirection, playerNumber);
+        }
+    }
+
+    private static List<TraversalNode> getNodesFromPlayer(Node currentNode, int playerNumber, List<TraversalNode> runningList) {
+        if(currentNode == null) {
+            return runningList;
+        } else {
+            if(currentNode.getPlayer() == playerNumber) {
+                runningList.add(new TraversalNode(currentNode));
+            }
+            for(Node node : currentNode.neighbors) {
+                return getNodesFromPlayer(node, playerNumber, runningList);
+            }
+        }
+        return null;
+    }
+
+    private static class TraversalNode {
+        public boolean isChecked;
+        public Node node;
+
+        public TraversalNode(Node node) {
+            this.node = node;
+            isChecked = false;
+        }
     }
 }
